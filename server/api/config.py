bc81d0d92fb41690bc774aafd53562d2eaa2a865
VihangaSathsaranieThalangedara <119300736+VihangaST@users.noreply.github.com>
2025-04-12 15:21:36 +0530

    modify config.py

--- server/api/config.py
+++ server/api/config.py
@@ -4,13 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 class Config:
-    # password = quote_plus('VSt@99')
-    
-    # SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:{password}@localhost/quizee'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    # PRIVATE_KEY_PATH = os.path.join(BASE_DIR, 'authentication', 'private_key.pem')
-    # PUBLIC_KEY_PATH = os.path.join(BASE_DIR, 'authentication', 'public_key.pem')
     db_user = os.getenv("DB_USERNAME")
     db_password = quote_plus(os.getenv("DB_PASSWORD"))
     db_name = os.getenv("DB_NAME")
